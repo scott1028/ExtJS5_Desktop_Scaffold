0dafbb386d62e2ecaff1a17dff3e9c42cbcd50c4
scott.lan <scott.lan@taisys.com>
2014-08-13 14:13:06 +0800

    update model & store & grid binding code

--- SSD_Web_Demo/app/model/User.js
+++ SSD_Web_Demo/app/model/User.js
@@ -1,13 +1,17 @@
 Ext.define('SSD_Web.model.User', {
     extend: 'Ext.data.Model',
     
-    fields: [
-        { name: 'username', type: 'string' },
-        { name: 'user_id', type: 'string' }
-    ],
+    // fields: [
+    //     { name: 'username', type: 'string' },
+    //     { name: 'user_id', type: 'string' },
+    // ],
 
     proxy: {
         type: 'rest',
-        url : '/api/privilege/system_user/'
+        url : '/api/privilege/system_user/',
+        reader: {
+            rootProperty: 'objects',
+            type: 'json',
+        }
     }
 });
--- SSD_Web_Demo/app/store/User.js
+++ SSD_Web_Demo/app/store/User.js
@@ -1,16 +1,9 @@
 // Chrome has a bug store.data will return [undefined * 8],
 // but store.data.items is correct data array.
 
+// as a model preload container
 Ext.define('SSD_Web.store.User', {
     extend: 'Ext.data.Store',
     model: 'SSD_Web.model.User',
-    autoLoad: true,
-    proxy: {
-        type: 'rest',
-        url: '/api/privilege/system_user/',
-        reader: {
-            rootProperty: 'objects',
-            type: 'json',
-        }
-    }
+    autoLoad: true
 });
