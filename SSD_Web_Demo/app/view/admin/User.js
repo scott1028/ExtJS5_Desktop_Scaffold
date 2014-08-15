e3eac3acdaab3bf387dee4148d81942a42fb71b2
scott.lan <scott.lan@taisys.com>
2014-08-15 12:03:27 +0800

    update datepicker

--- SSD_Web_Demo/app/view/admin/User.js
+++ SSD_Web_Demo/app/view/admin/User.js
@@ -26,8 +26,51 @@ Ext.define("SSD_Web.view.admin.User",{
                 { width: 'auto', header: 'LastLogin', dataIndex: 'last_login',
                   editor: {
                     xtype: 'datefield',
+                    // xtype: 'textfield',
                     allowBlank: false,
                     format: 'Y-m-d H:i:s',
+                    createPicker: function(){
+                        var me = this,
+                        format = Ext.String.format;
+
+                        var fn = Ext.picker.Date.prototype.renderTpl.pop();
+
+                        Ext.picker.Date.prototype.renderTpl.push('<div>789789</div>');
+
+                        Ext.picker.Date.prototype.renderTpl.push(fn);
+
+                        var dateTimePicker =  Ext.create('Ext.picker.Date',{
+                            pickerField: me,
+                            renderTo: document.body,
+                            floating: true,
+                            hidden: true,
+                            focusOnShow: true,
+                            minDate: me.minValue,
+                            maxDate: me.maxValue,
+                            disabledDatesRE: me.disabledDatesRE,
+                            disabledDatesText: me.disabledDatesText,
+                            disabledDays: me.disabledDays,
+                            disabledDaysText: me.disabledDaysText,
+                            format: me.format,
+                            showToday: me.showToday,
+                            startDay: me.startDay,
+                            minText: format(me.minText, me.formatDate(me.minValue)),
+                            maxText: format(me.maxText, me.formatDate(me.maxValue)),
+                            listeners: {
+                                scope: me,
+                                select: me.onSelect
+                            },
+                            keyNavConfig: {
+                                esc: function() {
+                                    me.collapse();
+                                    me.focus();
+                                }
+                            },
+                            renderTpl: Ext.picker.Date.prototype.renderTpl
+                        });
+
+                        return dateTimePicker;
+                    }
                   },
                   xtype:'datecolumn',
                   format: 'Y-m-d H:i:s'
